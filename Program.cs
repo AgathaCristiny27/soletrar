c4fd942d2358aa5f9e85b571245b4468281c675e
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-03-22 20:02:09 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+ 
+Console.WriteLine("--- Soletrando ---\n");
+Console.Write("digite um texto a ser soletrado: ");
+ 
+string texto = Console.ReadLine()!;
+ 
+Console.WriteLine("\nSoletrando fica...\n");
+ 
+Console.WriteLine(
+  String.Join("-",texto.ToUpper().ToCharArray())  
+);
+Console.WriteLine();
